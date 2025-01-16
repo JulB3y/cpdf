f6c55883b00c1243708d94489ecd042b2d03daf6
JulBey <git@julbey.org>
2025-01-16 19:50:27 +0100

    Initial Commit

---
+++ cpdf/cpdfApp.swift
@@ -0,0 +1,17 @@
+//
+//  cpdfApp.swift
+//  cpdf
+//
+//  Created by Julius Beyer on 16.01.25.
+//
+
+import SwiftUI
+
+@main
+struct cpdfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ cpdfTests/cpdfTests.swift
@@ -0,0 +1,17 @@
+//
+//  cpdfTests.swift
+//  cpdfTests
+//
+//  Created by Julius Beyer on 16.01.25.
+//
+
+import Testing
+@testable import cpdf
+
+struct cpdfTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
